8cd5f2ac80e98c52d29f46b0b72781ace996b057
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2024-03-16 06:13:04 +0000

    add installation script

---
+++ install_requirements.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "HELLO"
+
+pip install -r requirements.txt
+pip install --no-cache-dir git+https://github.com/Lightning-AI/pytorch-lightning.git@master
+pip install -U lightning-cloud
+pip install mosaicml
